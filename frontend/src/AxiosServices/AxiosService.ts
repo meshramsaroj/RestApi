8e41249fc19291bdc9abbd0a60a2a24d92606e33
sarojmeshram <saroj.g@morressier.com>
2026-05-26 19:03:29 +0530

    Added CORS issue fix and integrated backend with frontend

---
+++ frontend/src/AxiosServices/AxiosService.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+
+export const getUsers = async () => {
+    const response = await axios.get("/api/users");
+    console.log(response.data);
+    return response.data;
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Proxy API requests to the backend server to fix CORS issues during development
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000",
+      // '/api': 'https://restapi-fipo.onrender.com',
+    },
+  },
   plugins: [react()],
-})
+});
